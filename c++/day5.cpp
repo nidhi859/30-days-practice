a36a576c9a1762965cf116c6f0c28de05821b190
Nidhi Sharma <nidhisharma5241@gmail.com>
2020-04-14 23:38:42 +0530

    loops

---
+++ c++/day5.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    
+    int a;
+    int result;
+
+    cin >> a;
+
+    for ( int i = 1 ; i < 11 ; ++i ) {
+
+        result = a * i ;
+        cout << a << " x " << i << " = " << result << endl ;
+        }
+
+    return 0;
+}
+
