8127c7f20a8f172a1146d3b61f3430031d099ce8
Nidhi Sharma <nidhisharma5241@gmail.com>
2020-04-14 23:35:03 +0530

    conditions

---
+++ c++/day2.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+  
+     int n;
+
+    cin >> n;
+
+    if( n >=1 && n <= 100 )   
+
+    {
+        if( n%2 != 0 )   
+            cout << "Weird";
+
+        else {                
+            if( n >=2 && n <= 5 )
+                cout << "Not Weird";
+
+            else
+                if( n >=6 && n <=20 )
+                    cout << "Weird";
+
+            else
+                if ( n > 20)
+                    cout << "Not Weird";
+            }
+
+    }
+
+    return 0;
+
+}
+
